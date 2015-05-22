e1b4483fc497524d8e758ef65721361660c57c3e
Zander Baldwin <hello@zanderbaldwin.com>
2015-05-22 12:29:16 +0100

    Update ReflectionClass
    Update the ReflectionClass extension so that it passes tests.

--- src/Reflection/ReflectionClass.php
+++ src/Reflection/ReflectionClass.php
@@ -23,6 +23,11 @@ class ReflectionClass extends \ReflectionClass
     public function __construct($class, $relative)
     {
         parent::__construct($class);
+        if (!is_string($relative)) {
+            throw new \InvalidArgumentException(
+                'The second parameter specifying the relative position of the class must be a string.'
+            );
+        }
         $this->relative = trim(preg_replace('#/{2,}#', '/', strtr($relative, '\\', '/')), '/');
     }
 
@@ -45,6 +50,6 @@ class ReflectionClass extends \ReflectionClass
      */
     public function getRelativeNamespace()
     {
-        return strtr($this->getRelative(), '/', '\\');
+        return strtr($this->relative, '/', '\\');
     }
 }
